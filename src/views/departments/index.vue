e1d864aa716982a4af0ee8496cea24db1fc8281e
Tianhaitao <2804622087@qq.com>
2022-07-06 21:32:53 +0800

    将树形内容单独抽离组件

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -8,86 +8,35 @@
         align="middle"
         style="height: 40px"
       >
-        <el-col>
-          <span>江苏传智播客教育科技股份有限公司</span>
-        </el-col>
-        <el-col :span="4">
-          <el-row
-            type="flex"
-            justify="end"
-          >
-            <!-- 两个内容 -->
-            <el-col>负责人</el-col>
-            <el-col>
-              <!-- 下拉菜单 element -->
-              <el-dropdown>
-                <span>
-                  操作<i class="el-icon-arrow-down" />
-                </span>
-                <!-- 下拉菜单 -->
-                <el-dropdown-menu slot="dropdown">
-                  <el-dropdown-item>添加子部门</el-dropdown-item>
-                </el-dropdown-menu>
-              </el-dropdown>
-            </el-col>
-          </el-row>
-        </el-col>
+        <tree-tool
+          :tree-node="company"
+          :is-root="true"
+        />
       </el-row>
       <el-tree
         :data="departs"
         :props="defaultProps"
       >
-        <el-row
+        <tree-tool
           slot-scope="{ data }"
-          type="flex"
-          justify="space-between"
-          align="middle"
-          style="height: 40px; width: 100%"
-        >
-          <el-col>
-            <!-- 左侧内容 -->
-            <span>{{ data.name }}</span>
-          </el-col>
-          <el-col :span="4">
-            <el-row
-              type="flex"
-              justify="end"
-            >
-              <el-col>{{ data.manager }}</el-col>
-              <el-col>
-                <!-- 放置下拉菜单 -->
-                <el-dropdown>
-                  <!-- 内容 -->
-                  <span>操作
-                    <i class="el-icon-arrow-down" />
-                  </span>
-                  <!-- 具名插槽 -->
-                  <el-dropdown-menu slot="dropdown">
-                    <!-- 下拉选项 -->
-                    <el-dropdown-item>添加子部门</el-dropdown-item>
-                    <el-dropdown-item>编辑部门</el-dropdown-item>
-                    <el-dropdown-item>删除部门</el-dropdown-item>
-
-                  </el-dropdown-menu>
-                </el-dropdown>
-              </el-col>
-            </el-row>
-
-            <!-- 右侧内容 -->
-          </el-col>
-        </el-row>
+          :tree-node="data"
+        />
       </el-tree>
     </el-card>
   </div>
 </template>
 
 <script>
+import treeTool from '../components/tree-tools.vue'
 export default {
   name: 'OrganizationalStructure',
-  components: {},
+  components: {
+    treeTool
+  },
   props: {},
   data () {
     return {
+      company: { name: '江苏传智播客教育科技股份有限公司', manager: '负责人' },
       defaultProps: {
         label: 'name'
       },
