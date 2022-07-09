e11c89ed44866bbe2c658adf92072eb691e495db
Tianhaitao <2804622087@qq.com>
2022-07-09 16:44:17 +0800

    员工导入组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,11 @@
 // global identical registration
 import PageTools from './PageTools'
+import UploadExcel from './UploadExcel'
 export default {
   install(Vue) {
+    // registration tool component
     Vue.component('PageTools', PageTools)
+    // registration excel component
+    Vue.component('UploadExcel', UploadExcel)
   }
 }
